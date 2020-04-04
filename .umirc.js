30c205696c54925a14768dc1e38466cf92bb6c2d
欧舟 <wb-oz389525@alibaba-inc.com>
2020-04-04 22:36:39 +0800

    fix

--- .umirc.js
+++ .umirc.js
@@ -1,5 +1,7 @@
 export default {
-  base: "/pingos-player/",
+  history: { type: "hash" },
+  publicPath: "/pingos-player/",
+  hash: true,
   theme: {
     "@border-radius-base": "0px",
     "primary-color": "#2e85ed",
