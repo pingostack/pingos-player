6265928cdfeeb0bb33459d1eb353a91b2d8527c8
欧舟 <wb-oz389525@alibaba-inc.com>
2020-04-05 23:36:51 +0800

    fix

--- src/pages/flv/index.js
+++ src/pages/flv/index.js
@@ -2,42 +2,24 @@ import React, { useEffect, useRef, useState } from "react";
 import { Button, Input, Card } from "antd";
 import styles from "./index.less";
 
+import WXInlinePlayer from "./components/p";
+
 export default () => {
   const videoRef = useRef();
-  const hlsRef = useRef();
+  const playerRef = useRef();
   const [playing, setPlaying] = useState(false);
   const [url, setUrl] = useState(
-    "https://meet-frame.inside.xiaoeknow.com/app5ace18276a3e6_9kJzSUh6/video/1/l_5ae13437e845a_eoKpS8BMdV.m3u8"
+    "http://cyberplayer.bcelive.com/videoworks/mda-kbuhu4wqdi08dwix/cyberplayer/flv/cyberplayer-demo.flv"
   );
 
   useEffect(() => {
     if (WXInlinePlayer.isSupport()) {
       WXInlinePlayer.init({
-        asmUrl: "/prod.baseline.asm.combine.js",
-        wasmUrl: "/prod.baseline.wasm.combine.js",
-      });
-
-      WXInlinePlayer.ready().then(() => {
-        const player = new WXInlinePlayer({
-          url: "https://static.petera.cn/mm.flv",
-          $container: videoRef.current,
-          hasVideo: true,
-          hasAudio: true,
-          volume: 1.0,
-          muted: false,
-          autoplay: true,
-          loop: true,
-          isLive: false,
-          chunkSize: 128 * 1024,
-          preloadTime: 5e2,
-          bufferingTime: 1e3,
-          cacheSegmentCount: 64,
-          customLoader: null,
-        });
-
-        player.play();
+        asmUrl: `${window.routerBase}WXInlinePlayer/prod.baseline.asm.combine.js`,
+        wasmUrl: `${window.routerBase}WXInlinePlayer/prod.baseline.wasm.combine.js`,
       });
     }
+
     return () => {
       console.log("componentWillUnmount: 组件卸载， 做一些清理工作");
     };
@@ -45,18 +27,35 @@ export default () => {
 
   const play = () => {
     setPlaying(true);
-    // const hls = new Hls();
-    // hlsRef.current = hls;
-    // hls.attachMedia(videoRef.current);
-    // hls.loadSource(url);
-    // hls.on(Hls.Events.MANIFEST_PARSED, function() {
-    //   videoRef.current.play();
-    // });
+
+    WXInlinePlayer.ready().then(() => {
+      const player = new WXInlinePlayer({
+        url,
+        $container: videoRef.current,
+        hasVideo: true,
+        hasAudio: true,
+        volume: 1.0,
+        muted: false,
+        autoplay: true,
+        loop: true,
+        isLive: false,
+        chunkSize: 128 * 1024,
+        preloadTime: 5e2,
+        bufferingTime: 1e3,
+        cacheSegmentCount: 64,
+        customLoader: null,
+      });
+
+      player.play();
+
+      playerRef.current = player;
+    });
   };
 
   const stop = () => {
     setPlaying(false);
-    // hlsRef.current.destroy();
+    // playerRef.current.destroy();
+    // playerRef.current = null;
   };
 
   return (
